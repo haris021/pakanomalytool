50f9279449044e2c5d1af77106b29abcea508d7e
haris021 <harismushtaq21@yahoo.com>
2023-10-28 23:08:31 +0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -22,13 +22,10 @@ import calendar
 
 
 
-json_data = st.secrets["json_data"]
-#service_account = st.secrets["service_account"]
+json_object = st.secrets["json_data"]
+service_account = st.secrets["service_account"]
 
-# Preparing values
-json_object = json.loads(json_data, strict=False)
-service_account = json_object['client_email']
-json_object = json.dumps(json_object)
+
 # Authorising the app
 credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
 ee.Initialize(credentials)
